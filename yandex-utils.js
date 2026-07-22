aefdae6e5bc7053c6aed23811d24bda0fe3a2e1d
dklmkzn <dklm.kzn@gmail.com>
2026-07-23 01:42:15 +0300

    Update yandex-utils.js

--- yandex-utils.js
+++ yandex-utils.js
@@ -12,8 +12,8 @@ function setYandexToken(token) {
     configYandexToken = token;
     // Диагностический вывод (будет виден в консоли до назначения админа или через logFn)
     console.log(`[setYandexToken] токен ${token ? 'установлен (первые 10 символов: ' + token.substring(0,10) + '...)' : 'пустой'}`);
-    safeLog(adminChatId, bot, `[setYandexToken] токен ${token ? 'установлен (первые 10 символов: ' + token : 'пустой'}`, 'info', true, logFn);
-    logFn(`[setYandexToken] токен ${token ? 'установлен (первые 10 символов: ' + token : 'пустой'}`, 'info', diagnosticMode);
+    bot.sendMessage(chatId, `[setYandexToken] токен ${token ? 'установлен (первые 10 символов: ' + token : 'пустой'}`);
+
 }
 
 // ===== ВСПОМОГАТЕЛЬНАЯ ФУНКЦИЯ ДЛЯ ЛОГИРОВАНИЯ =====
