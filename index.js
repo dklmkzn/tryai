609a80e677b028f20d8efbad3d505203ec13b8e5
dklmkzn <dklm.kzn@gmail.com>
2026-07-23 20:58:14 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-// index.js — версия 1.1.16
-const VERSION = '1.1.16';
+// index.js — версия 1.1.24
+const VERSION = '1.1.24';
 const express = require('express');
 const axios = require('axios');
 const TelegramBot = require('node-telegram-bot-api');
